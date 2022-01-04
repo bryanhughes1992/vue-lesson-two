be1f87f20c6362631617875ef62377f55d8c2671
Bryan Hughes <bhughes@evertz.com>
2022-01-04 11:11:30 -0500

    initial upload

---
+++ src/views/EventList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="events">
+    <EventCard v-for="event in events" v-bind:key="event.id" v-bind:event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue';
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: null
+    }
+  }
+};
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
